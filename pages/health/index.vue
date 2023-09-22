c9e7b7253b1893cdcd93833794792f2fa630c251
pandadev666 <smartguy.panda666@gmail.com>
2023-09-21 18:32:29 -0700

    health page frontend

---
+++ pages/health/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <NuxtLayout>
+    <template #content>
+      <p class="text-3xl font-bold tracking-wide mb-8">Health</p>
+      <div class="tracking-wide rounded-lg px-8 py-4 border border-[#D9D9D9] text-black flex flex-col w-full">
+        <div class="health-table-header">
+          <div class="w-[8%]">
+            Source
+          </div>
+          <div class="w-[8%]">
+            Dest
+          </div>
+          <div class="w-[5%]">
+            Retry
+          </div>
+          <div class="w-[9%]">
+            ExecTime
+          </div>
+          <div class="w-[20%]">
+            DestConfig
+          </div>
+          <div class="w-[25%]">
+            ErrorResponse
+          </div>
+          <div class="w-[25%]">
+            Payload
+          </div>
+        </div>
+        <div class="health-table-body">
+          <HealthPageItem />
+          <HealthPageItem />
+          <HealthPageItem />
+          <HealthPageItem />
+          <HealthPageItem />
+          <HealthPageItem />
+          <HealthPageItem />
+          <HealthPageItem />
+        </div>
+      </div>
+    </template>
+  </NuxtLayout>
+</template>
+<script>
+import HealthPageItem from "@/components/HealthPage/Item.vue"
+export default {
+  name: "health",
+  components: {HealthPageItem},
+}
+</script>
+<style scoped>
+.health-table-header {
+  @apply flex flex-row mb-4
+}
+.health-table-header div {
+  @apply text-lg font-bold
+}
+</style>
