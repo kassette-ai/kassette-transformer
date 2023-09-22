327b1a8f9f92b3e9f0211e8bb0ef5bd57a268402
pandadev666 <smartguy.panda666@gmail.com>
2023-09-22 08:11:26 -0700

    health-page backend integration

--- components/HealthPage/Item.vue
+++ components/HealthPage/Item.vue
@@ -1,21 +1,34 @@
 <template>
     <div class="item-wrapper">
-        <div class="max-w-[8%] w-[8%]">Camunda</div>
-        <div class="max-w-[8%] w-[8%]">Anaplan</div>
-        <div class="max-w-[5%] w-[5%]">8</div>
-        <div class="max-w-[9%] w-[9%]">12/09/2023 9:20</div>
-        <div class="max-w-[20%] w-[20%]">{Config}{Config}{Config}{Config}{Config}{Config}{Config}{Config}{Config}</div>
-        <div class="max-w-[25%] w-[25%]">Invalid Auth Key</div>
-        <div class="max-w-[25%] w-[25%]">[Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload][Payload]</div>
+        <div class="max-w-[8%] w-[8%]">{{ data.source_name }}</div>
+        <div class="max-w-[8%] w-[8%]">{{ data.destination_name}}</div>
+        <div class="max-w-[5%] w-[5%] text-center">{{ data.attempt_num}}</div>
+        <div class="max-w-[9%] w-[9%]">{{ data.exec_time }}</div>
+        <div class="max-w-[20%] w-[20%]">{{ json_to_string(data.destination_config) }}</div>
+        <div class="max-w-[25%] w-[25%]">{{ json_to_string(data.error_response) }}</div>
+        <div class="max-w-[25%] w-[25%]">{{ json_to_string(data.payload) }}</div>
     </div>
 </template>
 <script>
 export default {
     name: "HealthPageItem",
+    props: ["data"],
+    methods: {
+        json_to_string(jsonData) {
+            if (jsonData == null) {
+                return ""
+            } else {
+                return JSON.stringify(jsonData)
+            }
+        }
+    }
 }
 </script>
 <style scoped>
 .item-wrapper {
     @apply flex flex-row my-3
+}
+.item-wrapper div {
+    @apply break-all px-2 h-[200px] overflow-y-auto
 }
 </style>
